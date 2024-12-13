27b56dcb27c8ee02d3c777587e2eecbf3b9ef6b4
Student-shivamsharma <sgivamsharma06@gmail.com>
2024-12-13 22:55:17 +0530

    git

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-black h-screen w-screen text-white'>
+      Navbar
+    </div>
+  )
+}
+
+export default Navbar
